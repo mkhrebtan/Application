41a41bcfb7dd35170a4e3e3dd44bbb1a14179edc
Maksym Khrebtan <m.khrebtan21@gmail.com>
2025-10-17 22:42:53 +0300

    Implement create event form with validation and UI enhancements

--- src/Web/src/app/pages/create-event/create-event.ts
+++ src/Web/src/app/pages/create-event/create-event.ts
@@ -1,15 +1,50 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { HeroSection } from '../../layout/hero-section/hero-section';
+import { AbstractControl, FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-create-event',
-  imports: [],
-  template: `
-    <p>
-      create-event works!
-    </p>
-  `,
-  styles: ``
+  imports: [HeroSection, ReactiveFormsModule, CommonModule],
+  templateUrl: './create-event.html',
+  styles: ``,
 })
 export class CreateEvent {
+  protected readonly heroTitle = 'Create New Event';
+  protected readonly heroDescription = 'Fill in the details below to create a new event.';
 
+  private readonly formBuilder = inject(FormBuilder);
+  protected readonly eventCreationForm = this.formBuilder.group({
+    title: ['', [Validators.required, Validators.maxLength(100)]],
+    description: ['', [Validators.required, Validators.maxLength(500)]],
+    date: ['', [Validators.required]],
+    location: ['', [Validators.required, Validators.maxLength(200)]],
+    capacity: [null, [Validators.min(1)]],
+    visibility: ['public', [Validators.required]],
+  });
+
+  get title(): AbstractControl | null {
+    return this.eventCreationForm.get('title');
+  }
+  get description(): AbstractControl | null {
+    return this.eventCreationForm.get('description');
+  }
+  get date(): AbstractControl | null {
+    return this.eventCreationForm.get('date');
+  }
+  get location(): AbstractControl | null {
+    return this.eventCreationForm.get('location');
+  }
+  get capacity(): AbstractControl | null {
+    return this.eventCreationForm.get('capacity');
+  }
+
+  onSubmit() {
+    if (this.eventCreationForm.valid) {
+      const formData = this.eventCreationForm.value;
+      console.log('Event Created:', formData);
+    } else {
+      console.log('Form is invalid');
+    }
+  }
 }
