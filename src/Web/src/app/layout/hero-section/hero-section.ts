5659f3802e483cf4b1bb9959de5cc06a3dade1a1
Maksym Khrebtan <m.khrebtan21@gmail.com>
2025-10-17 22:43:03 +0300

    Update hero section alignment for improved responsiveness

--- src/Web/src/app/layout/hero-section/hero-section.ts
+++ src/Web/src/app/layout/hero-section/hero-section.ts
@@ -4,7 +4,7 @@ import { Component, Input } from '@angular/core';
   selector: 'app-hero-section',
   imports: [],
   template: `
-    <section class="flex flex-col gap-5 text-left">
+    <section class="flex flex-col gap-5 text-center md:text-left">
       <h1 class="text-5xl font-semibold">{{ title }}</h1>
       <p class="text-gray-600 text-md">
         {{ description }}
