aa0bbd3b1ba68e02951d2877063c4ffb512ce301
Maksym Khrebtan <m.khrebtan21@gmail.com>
2025-10-18 10:54:15 +0300

    Fix form input id and for attributes to use input parameter

--- src/Web/src/app/shared/components/form-input/form-input.ts
+++ src/Web/src/app/shared/components/form-input/form-input.ts
@@ -7,14 +7,14 @@ import { NgClass } from '@angular/common';
   imports: [FormsModule, ReactiveFormsModule, NgClass],
   template: `
     <div class="flex flex-col gap-2">
-      <label for="title" class="text-sm font-medium text-gray-700">
+      <label [for]="id" class="text-sm font-medium text-gray-700">
         {{ label }}
-        @if (control?.hasValidator(Validators.required)) {
+        @if (control.hasValidator(Validators.required)) {
           <span class="text-red-500">*</span>
         }
       </label>
       <input
-        id="title"
+        [id]="id"
         [formControl]="formControl"
         [type]="type"
         [placeholder]="placeholder"
@@ -22,10 +22,10 @@ import { NgClass } from '@angular/common';
         class="w-full py-2 px-3 placeholder:text-gray-400 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-1 focus:ring-gray-400 focus:border-gray-400 transition-colors"
         [ngClass]="{
           'border-red-500 focus:border-red-500 focus:ring-red-500':
-            control?.invalid && control?.touched,
+            control.invalid && control.touched,
         }"
       />
-      @if (control?.invalid && control?.touched) {
+      @if (control.invalid && control.touched) {
         <div class="text-red-500 text-xs mt-1">
           <ng-content select="[error-message]"></ng-content>
         </div>
