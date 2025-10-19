b429d5bb2565c834b77e7d09d315dafad7983e18
Maksym Khrebtan <m.khrebtan21@gmail.com>
2025-10-19 23:42:22 +0300

    Add UpdateEventCapacity command, handler, and validator

---
+++ src/Application/Commands/Events/UpdateCapacity/UpdateEventCapacityCommand.cs
@@ -0,0 +1,5 @@
+﻿using Application.Abstraction.Mediator;
+
+namespace Application.Commands.Events.UpdateCapacity;
+
+public record UpdateEventCapacityCommand(Guid EventId, int? Capacity) : ICommand;
---
+++ src/Application/Commands/Events/UpdateCapacity/UpdateEventCapacityCommandValidator.cs
@@ -0,0 +1,16 @@
+﻿using FluentValidation;
+
+namespace Application.Commands.Events.UpdateCapacity;
+
+internal sealed class UpdateEventCapacityCommandValidator : AbstractValidator<UpdateEventCapacityCommand>
+{
+    public UpdateEventCapacityCommandValidator()
+    {
+        RuleFor(x => x.EventId)
+            .NotEmpty().WithMessage("EventId is required.");
+
+        RuleFor(x => x.Capacity)
+            .GreaterThan(0)
+            .When(x => x.Capacity.HasValue).WithMessage("Capacity must be greater than zero if specified.");
+    }
+}
