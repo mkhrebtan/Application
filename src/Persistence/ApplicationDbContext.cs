0d2c1c159f760768a32848180de9d2453d89fee3
Maksym Khrebtan <m.khrebtan21@gmail.com>
2025-10-20 13:13:02 +0300

    Refactor ApplicationDbContext and add dependency injection for application, infrastructure, and persistence layers

--- src/Persistence/ApplicationDbContext.cs
+++ src/Persistence/ApplicationDbContext.cs
@@ -5,11 +5,6 @@ namespace Persistence;
 
 public class ApplicationDbContext : DbContext
 {
-    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
-        : base(options)
-    {
-    }
-
     public DbSet<User> Users { get; set; }
 
     public DbSet<Event> Events { get; set; }
---
+++ src/Persistence/DependencyInjection.cs
@@ -0,0 +1,24 @@
+﻿using Domain.Abstraction.Interfaces;
+using Domain.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Persistence.Repositories;
+
+namespace Persistence;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddPersistence(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddDbContext<ApplicationDbContext>(options => options.UseNpgsql(configuration["CONNECTION_STRING"]));
+        services.AddScoped<IUnitOfWork, UnitOfWork>();
+        services.AddScoped(typeof(IRepository<>), typeof(GenericRepository<>));
+        services.AddScoped<IEventRepository, EventRepository>();
+        services.AddScoped<IUserRepository, UserRepository>();
+        services.AddScoped<IEventParticipantRepository, EventParticipantRepository>();
+        services.AddScoped<IRefreshTokenRepository, RefreshTokenRepository>();
+
+        return services;
+    }
+}
